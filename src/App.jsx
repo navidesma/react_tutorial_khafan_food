8b3b2dfc7e0359b33ec16b46c4cbd18a17efa164
navidesma <esma.navid@gmail.com>
2024-06-07 12:10:34 +0330

    start of react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
 
 function App() {
-  return <Home/>;
+  return <>
+    <Routes>
+      <Route path="home" element={<Home/>} />
+      <Route path="shopping-cart" element={<ShoppingCart/>} />
+    </Routes>
+  </>;
 }
 
 export default App;
---
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,5 @@
+export default function ShoppingCart() {
+    return (
+        <div><h1>Shopping Cart</h1></div>
+    )
+}
